21323c5cad8367c8ca13f333725c91d67ee75371
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-06-16 15:55:14 -0600

    Added fetching and retrieval of data from API into eventListener

--- script.js
+++ script.js
@@ -8,45 +8,53 @@ let currentWeather = "";
 let lowTemp = "";
 let highTemp = "";
 
-// Attatch event listener to the submit button to extract the user entered city name
-// TODO: Add fetchData functions to extract data and run in the eventlistener as well
+// Attatch event listener to the submit button
 document.getElementById("submit").addEventListener("click", () => {
+  // Obtain the cityName from user input
   let cityName = document.getElementById("city-name-input").value;
-  city = cityName.toLowerCase();
+  city = cityName.toLowerCase(); // convert cityName to lower case
+
+  // Call the fetchWeatherData funciton
+  fetchWeatherData(city);
 });
 
-// Fetch data from the openWeather api
+// Define function to extract data from the openweathermap.org API and return the data
+function fetchWeatherData(city) {
+  // Set the openweathermap.org API Key
+  let apiKey = "f7cac34aa355b8f5cc5fa7c3833a0015";
+  let units = "imperial"; // standard (kelvin), metric (celcius), imperial (farenheight)
 
-// Set the openweathermap.org API Key
-let apiKey = "f7cac34aa355b8f5cc5fa7c3833a0015";
-let units = "imperial"; // standard (kelvin), metric (celcius), imperial (farenheight)
-city = "chicago"; // TODO: Set a temporary value for city
-let weatherPromise = fetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`
-);
+  // Call the fetch function on th openweathermap API
+  let weatherPromise = fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`
+  );
 
-// Parse the weather information and extract the relevant outputs
-weatherPromise
-  .then((response) => {
-    return response.json();
-  })
-  .then((resData) => {
-    console.log("Data Parsed: ...", resData);
+  // Parse the weather information and extract the relevant outputs
+  weatherPromise
+    .then((response) => {
+      return response.json();
+    })
+    .then((resData) => {
+      // console.log("Data Parsed: ...", resData);
+      document.getElementById("city-name-output").innerHTML =
+        city.toUpperCase();
 
-    weatherState = resData.weather[0].main;
-    console.log(weatherState);
+      weatherState = resData.weather[0].main;
+      document.getElementById("weather-state").innerHTML = weatherState;
 
-    currentWeather = resData.main.temp;
-    console.log(currentWeather);
+      currentWeather = resData.main.temp;
+      document.getElementById("weather-current").innerHTML =
+        "<strong>" + String(currentWeather) + "&#176F" + "</strong>";
 
-    lowTemp = resData.main.temp_min;
-    console.log(lowTemp);
+      lowTemp = resData.main.temp_min;
+      document.getElementById("weather-low").innerHTML =
+        String(lowTemp) + "&#176F";
 
-    highTemp = resData.main.temp_max;
-    console.log(highTemp);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-// Display the weather information
+      highTemp = resData.main.temp_max;
+      document.getElementById("weather-high").innerHTML =
+        String(highTemp) + "&#176F";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
